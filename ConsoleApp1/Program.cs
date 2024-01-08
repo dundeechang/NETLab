95b911f17f679d653011ebe754f842bc0f3473bb
dundeechang <42328280+dundeechang@users.noreply.github.com>
2024-01-08 21:14:33 +0800

    feat: 初始化

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "國泰證券是一間好公司。";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            while (true)
+            {
+                Console.Clear();
+                Console.Write(text);
+                text = text[1..] + text[0];
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
